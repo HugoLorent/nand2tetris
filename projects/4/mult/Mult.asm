5ae3101d8997796521f79a84bcce25da984d3382
Hugo LORENT <hugo.lorent@protonmail.com>
2025-04-15 21:22:30 +0200

    implement multiplication program using repeated addition

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,43 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// Initialize R2 to 0
+@R2
+M=0
+
+// Check if R0 or R1 is 0
+@R0
+D=M
+@END
+D;JEQ    // If R0=0, goto END
+
+@R1
+D=M
+@END
+D;JEQ    // If R1=0, goto END
+
+// Initialize counter (using R1 as counter)
+@R1
+D=M
+@n
+M=D     // n = R1
+
+(LOOP)
+    // Add R0 to R2
+    @R0
+    D=M
+    @R2
+    M=D+M    // R2 = R2 + R0
+
+    // Decrement counter
+    @n
+    M=M-1
+    D=M
+
+    // If counter > 0, continue loop
+    @LOOP
+    D;JGT
+
+(END)
+    @END
+    0;JMP
